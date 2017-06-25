3d4143fd81f0f1b1e2d60860634ad2a30acf2045
toda <starfish719@gmail.com>
2017-06-25 11:53:23 +0900

    スコアを取得できるように対応

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const storage = require('@google-cloud/storage')({
 });
 
 const async = require('async');
+const httpcli = require('cheerio-httpcli');
 
 /**
  * Triggered from a message on a Cloud Storage bucket.
@@ -22,16 +23,55 @@ exports.processFile = function(event, callback) {
         callback();
     } else if (eventData.metageneration === '1') {
         async.waterfall([
-            function (waterCallback){
+            function (waterCallback) {
                 vision.detectText(file, function(err, text, apiResponse) {
-                    console.log('err:' + JSON.stringify(err));
+                    console.log('text:' + JSON.stringify(text));
 
-                    waterCallback(null, text);
+                    waterCallback(null);
                 });
             },
-            function (text, waterCallback) {
-                console.log('text:' + text);
-                waterCallback(null, "result");
+            function (waterCallback) {
+                vision.detectSimilar(file)
+                    .then((results) => {
+                        const webDetection = results[1].responses[0].webDetection;
+
+                        if (webDetection.pagesWithMatchingImages.length) {
+                            console.log(`Full matches found: ` + webDetection.pagesWithMatchingImages.length);
+                            
+                            for (var idx in webDetection.pagesWithMatchingImages) {
+                                const pageUrl = webDetection.pagesWithMatchingImages[idx].url;
+                                console.log(`  URL: ` + pageUrl);
+                                if (pageUrl.indexOf('https://tabelog.com/') === 0) {
+                                    waterCallback(null, pageUrl);
+                                    return;
+                                }
+                            }
+
+                            throw new Error('not found target url');
+                        } else {
+                            throw new Error('not found web detection');
+                        }
+                    })
+                    .catch((err) => {
+                        waterCallback(err);
+                    });
+            },
+            function (url, waterCallback) {
+                httpcli.fetch(url)
+                    .then(function (result) {
+                        if (result.err) {
+                            waterCallback(err, 'err');
+                        }
+
+                        const $ = result.$;
+                        const score = $('b.tb-rating__val.rdheader-rating__score-val span').text();
+                        console.error('restaulant score:' + score);
+
+                        waterCallback(null, 'success');
+                    })
+                    .catch((err) => {
+                        waterCallback(err);
+                    });
             },
         ], function (err, result) {
             if (err) {
